490aa8209091e7ff3f892a78849d10f566694a7f
David Simon Feldhofer <david@feldhofer.cc>
2025-09-22 17:52:27 +0200

    dev

---
+++ patterns/ziele-f%c3%bcr-dein-training.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Title: Ziele für dein Training
+ * Slug: fitness-salzkammergut/ziele-f%c3%bcr-dein-training
+ * Categories: 
+ */
+?>
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:fitness-salzkammergut/post-grid {"columns":2} /-->
