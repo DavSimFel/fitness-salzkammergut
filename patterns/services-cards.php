71d5653896e9867dab3f8f02ab3557f0c38b9ff6
David Simon Feldhofer <david@feldhofer.cc>
2025-09-22 18:36:16 +0200

    dev

---
+++ patterns/services-cards.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Title: Services / Dein Training cards
+ * Slug: fitness-salzkammergut/services-cards
+ * Categories: featured, columns
+ * Block Types: core/group
+ * Inserter: yes
+ */
+?>
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"24px","bottom":"24px"}}}} -->
+<div class="wp-block-group alignfull" style="padding-top:24px;padding-bottom:24px"><!-- wp:group {"align":"wide","layout":{"type":"constrained"},"style":{"spacing":{"blockGap":"24px"}}} -->
+<div class="wp-block-group alignwide"><!-- wp:heading {"level":2,"style":{"typography":{"fontWeight":"800","fontSize":"36px"}}} -->
+<h2 style="font-size:36px;font-weight:800">Dein Training</h2>
+<!-- /wp:heading -->
+
+<!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"className":"is-style-card","style":{"spacing":{"padding":{"top":"16px","bottom":"16px","left":"16px","right":"16px"},"blockGap":"12px"}}} -->
+<div class="wp-block-group is-style-card" style="padding-top:16px;padding-right:16px;padding-bottom:16px;padding-left:16px"><!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"22px","fontWeight":"700"}}} -->
+<h3 style="font-size:22px;font-weight:700">Kursprogramm</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Abwechslung mit Yoga, HIIT, Rückenfit & mehr – für Einsteiger und Fortgeschrittene.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"textColor":"almost-black","backgroundColor":"lime-light"} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-almost-black-color has-lime-light-background-color has-text-color has-background wp-element-button" href="/kursprogramm">Zu den Kursen</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"className":"is-style-card","style":{"spacing":{"padding":{"top":"16px","bottom":"16px","left":"16px","right":"16px"},"blockGap":"12px"}}} -->
+<div class="wp-block-group is-style-card" style="padding-top:16px;padding-right:16px;padding-bottom:16px;padding-left:16px"><!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"22px","fontWeight":"700"}}} -->
+<h3 style="font-size:22px;font-weight:700">Ziele erreichen</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Abnehmen, Muskulatur, Reha & Prävention, Fitness & Gesundheit, Sport Performance.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"textColor":"almost-black","backgroundColor":"lime-light"} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-almost-black-color has-lime-light-background-color has-text-color has-background wp-element-button" href="/ziele">Alle Ziele</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"className":"is-style-card","style":{"spacing":{"padding":{"top":"16px","bottom":"16px","left":"16px","right":"16px"},"blockGap":"12px"}}} -->
+<div class="wp-block-group is-style-card" style="padding-top:16px;padding-right:16px;padding-bottom:16px;padding-left:16px"><!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"22px","fontWeight":"700"}}} -->
+<h3 style="font-size:22px;font-weight:700">Tarife & Probetraining</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Basic, Flex, Young, Baby – plus Kurzzeit-Karten. Unverbindliches Probetraining jederzeit.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"lime-dark"} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-lime-dark-background-color has-background wp-element-button" href="/tarife#probetraining">Probetraining</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button" href="/tarife">Tarife ansehen</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
