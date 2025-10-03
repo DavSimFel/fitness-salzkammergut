0aac7482279938c9baa2976c8ac9238204f796b6
David Simon Feldhofer <david@feldhofer.cc>
2025-10-03 11:50:23 +0200

    dev

---
+++ patterns/linked-logo-row.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Title: Linked Logo Row
+ * Slug: fitness-salzkammergut/linked-logo-row
+ * Categories: 
+ */
+?>
+<!-- wp:group {"style":{"layout":{"selfStretch":"fit","flexSize":null},"dimensions":{"minHeight":"48px"},"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"},"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between","verticalAlignment":"center"}} -->
+<div class="wp-block-group" style="min-height:48px;margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:image {"lightbox":{"enabled":false},"width":"auto","height":"24px","sizeSlug":"full","linkDestination":"custom","style":{"spacing":{"margin":{"top":"0","bottom":"0"}},"layout":{"selfStretch":"fill","flexSize":null}}} -->
+<figure class="wp-block-image size-full is-resized" style="margin-top:0;margin-bottom:0"><a href="https://fitness-salzkammergut.at/studio/vivo-bad-goisern/"><img src="https://fitness-salzkammergut.at/wp-content/uploads/2025/10/Dachmarke_OnLight_vivo.svg" alt="" style="width:auto;height:24px"/></a></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"width":"auto","height":"32px","sizeSlug":"full","linkDestination":"none","style":{"spacing":{"margin":{"top":"0","bottom":"0","left":"0","right":"0"}},"layout":{"selfStretch":"fit","flexSize":null}}} -->
+<figure class="wp-block-image size-full is-resized" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0"><img src="https://fitness-salzkammergut.at/wp-content/uploads/2025/10/Dachmarke_OnLight_dot.svg" alt="" style="width:auto;height:32px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"lightbox":{"enabled":false},"width":"auto","height":"24px","sizeSlug":"full","linkDestination":"custom","style":{"spacing":{"margin":{"top":"0","bottom":"0","left":"0","right":"0"}},"layout":{"selfStretch":"fit","flexSize":null}}} -->
+<figure class="wp-block-image size-full is-resized" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0"><a href="https://fitness-salzkammergut.at/studio/excitingfit-bad-ischl/"><img src="https://fitness-salzkammergut.at/wp-content/uploads/2025/10/Dachmarke_OnLight_excitingfit.svg" alt="" style="width:auto;height:24px"/></a></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
