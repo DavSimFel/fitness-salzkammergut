415f1b2e87861de5d8cc91e011e6f58945beefaf
David Simon Feldhofer <david@feldhofer.cc>
2025-10-03 15:58:05 +0200

    dev

--- patterns/header.php
+++ patterns/header.php
@@ -5,11 +5,12 @@
  * Inserter: no
  */
 ?>
-<!-- wp:cover {"useFeaturedImage":true,"dimRatio":10,"isUserOverlayColor":true,"minHeight":100,"minHeightUnit":"vh","contentPosition":"center center","isDark":false,"sizeSlug":"full","align":"center","style":{"color":[]},"layout":{"type":"constrained"}} -->
-<div class="wp-block-cover aligncenter is-light" style="min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-10 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"dimensions":{"minHeight":"100vh"},"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"},"margin":{"top":"0","bottom":"0"},"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center","flexWrap":"nowrap","verticalAlignment":"top"}} -->
-<div class="wp-block-group" style="min-height:100vh;margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:group {"className":"style=\u0022position:sticky;top:0;z-index:1000\u0022","style":{"spacing":{"blockGap":"var:preset|spacing|60","padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center","verticalAlignment":"center"}} -->
-<div class="wp-block-group style=&quot;position:sticky;top:0;z-index:1000&quot;" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:group {"style":{"border":{"radius":"100px"},"spacing":{"padding":{"right":"var:preset|spacing|60","left":"var:preset|spacing|60"}}},"backgroundColor":"soft-white","layout":{"type":"flex","flexWrap":"nowrap"},"blockVisibility":{"controlSets":[{"id":1,"enable":true,"controls":{"screenSize":{"hideOnScreenSize":{"large":true,"medium":true}}}}]}} -->
-<div class="wp-block-group has-soft-white-background-color has-background" style="border-radius:100px;padding-right:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:group {"metadata":{"patternName":"fitness-salzkammergut/linked-logo-row","name":"Linked Logo Row"},"style":{"layout":{"selfStretch":"fit","flexSize":null},"dimensions":{"minHeight":"48px"},"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"},"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between","verticalAlignment":"center"}} -->
+<!-- wp:group {"backgroundColor":"almost-black","layout":{"type":"default"}} -->
+<div class="wp-block-group has-almost-black-background-color has-background"><!-- wp:cover {"useFeaturedImage":true,"dimRatio":50,"overlayColor":"almost-black","isUserOverlayColor":true,"minHeight":100,"minHeightUnit":"vh","contentPosition":"top center","isDark":false,"sizeSlug":"full","align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|soft-white"}},"heading":{"color":{"text":"var:preset|color|soft-white","background":"var:preset|color|custom-transparent"}}}},"textColor":"soft-white","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover aligncenter is-light has-custom-content-position is-position-top-center has-soft-white-color has-text-color has-link-color" style="min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-almost-black-background-color has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:group {"metadata":{"name":"Navigation"},"style":{"elements":{"link":{"color":{"text":"var:preset|color|black-90"}}}},"textColor":"black-90","layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group has-black-90-color has-text-color has-link-color"><!-- wp:group {"metadata":{"name":"Navigation Bar - Mobile"},"style":{"border":{"radius":"100px"},"spacing":{"padding":{"right":"var:preset|spacing|40","left":"var:preset|spacing|40"}}},"backgroundColor":"soft-white","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"},"blockVisibility":{"visibilityPresets":{"presets":[547]}}} -->
+<div class="wp-block-group has-soft-white-background-color has-background" style="border-radius:100px;padding-right:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:group {"metadata":{"patternName":"fitness-salzkammergut/linked-logo-row","name":"Linked Logo Row"},"style":{"layout":{"selfStretch":"fit","flexSize":null},"dimensions":{"minHeight":"48px"},"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"},"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between","verticalAlignment":"center"}} -->
 <div class="wp-block-group" style="min-height:48px;margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:image {"lightbox":{"enabled":false},"width":"auto","height":"24px","sizeSlug":"full","linkDestination":"custom","style":{"spacing":{"margin":{"top":"0","bottom":"0"}},"layout":{"selfStretch":"fill","flexSize":null}}} -->
 <figure class="wp-block-image size-full is-resized" style="margin-top:0;margin-bottom:0"><a href="https://fitness-salzkammergut.at/studio/vivo-bad-goisern/"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/Dachmarke_OnLight_vivo.svg" alt="" style="width:auto;height:24px"/></a></figure>
 <!-- /wp:image -->
@@ -21,12 +22,107 @@
 <!-- wp:image {"lightbox":{"enabled":false},"width":"auto","height":"24px","sizeSlug":"full","linkDestination":"custom","style":{"spacing":{"margin":{"top":"0","bottom":"0","left":"0","right":"0"}},"layout":{"selfStretch":"fit","flexSize":null}}} -->
 <figure class="wp-block-image size-full is-resized" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0"><a href="https://fitness-salzkammergut.at/studio/excitingfit-bad-ischl/"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/Dachmarke_OnLight_excitingfit.svg" alt="" style="width:auto;height:24px"/></a></figure>
 <!-- /wp:image --></div>
+<!-- /wp:group -->
+
+<!-- wp:navigation {"ref":430,"icon":"menu","layout":{"type":"flex","flexWrap":"nowrap"},"blockVisibility":{"controlSets":[{"id":1,"enable":true,"controls":{"screenSize":{"hideOnScreenSize":{"small":false,"medium":false}}}}]}} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Navigation Bar - Desktop/Tablet"},"style":{"border":{"radius":"100px"},"spacing":{"padding":{"right":"var:preset|spacing|60","left":"var:preset|spacing|60"}}},"backgroundColor":"soft-white","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"},"blockVisibility":{"visibilityPresets":{"presets":[557]}}} -->
+<div class="wp-block-group has-soft-white-background-color has-background" style="border-radius:100px;padding-right:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:navigation {"ref":430,"overlayMenu":"never","layout":{"type":"flex","flexWrap":"nowrap"},"blockVisibility":{"controlSets":[{"id":1,"enable":true,"controls":{"screenSize":{"hideOnScreenSize":{"small":false,"medium":false}}}}]}} /--></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
 
+<!-- wp:spacer {"height":"0px","style":{"layout":{"flexSize":"100px","selfStretch":"fixed"}}} -->
+<div style="height:0px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns {"verticalAlignment":null,"style":{"layout":{"selfStretch":"fit","flexSize":null}}} -->
+<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center","width":"50%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:50%"><!-- wp:heading {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"margin":{"top":"0","bottom":"0","left":"0","right":"0"}}},"fontSize":"4xl"} -->
+<h2 class="wp-block-heading has-4-xl-font-size" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><?php esc_html_e('Drinnen trainieren,', 'fitness-salzkammergut');?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"spacing":{"padding":{"top":"0","bottom":"0"},"margin":{"top":"0","bottom":"0","left":"0","right":"0"}}},"fontSize":"4xl","fontFamily":"tiempos-text"} -->
+<h2 class="wp-block-heading has-tiempos-text-font-family has-4-xl-font-size" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;padding-top:0;padding-bottom:0;font-style:normal;font-weight:600"><?php esc_html_e('draussen leben', 'fitness-salzkammergut');?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e('In unseren Fitness Studios in Bad Ischl und Bad Goisern findest du alle Möglichkeiten für ein professionelles Training – und das Wichtigste dabei: Du trainierst nicht allein.', 'fitness-salzkammergut');?></p>
+<!-- /wp:paragraph -->
+
 <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-<div class="wp-block-group"><!-- wp:navigation {"ref":430,"layout":{"type":"flex","flexWrap":"nowrap"},"blockVisibility":{"controlSets":[{"id":1,"enable":true,"controls":{"screenSize":{"hideOnScreenSize":{"small":false,"medium":false}}}}]}} /--></div>
+<div class="wp-block-group"></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"50%","layout":{"type":"constrained","justifyContent":"right"}} -->
+<div class="wp-block-column" style="flex-basis:50%"><!-- wp:group {"style":{"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap","justifyContent":"right"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"border":{"radius":"10px"},"spacing":{"blockGap":"var:preset|spacing|50","padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}},"elements":{"link":{"color":{"text":"var:preset|color|black-90"}}}},"backgroundColor":"black-30","textColor":"black-90","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group has-black-90-color has-black-30-background-color has-text-color has-background has-link-color" style="border-radius:10px;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--50)"><!-- wp:image {"width":"42px","height":"42px","scale":"cover","sizeSlug":"full","linkDestination":"none","style":{"spacing":{"margin":{"top":"0","bottom":"0","left":"0","right":"0"}}}} -->
+<figure class="wp-block-image size-full is-resized" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/Vivo_Symbol_Black.svg" alt="" class="" style="object-fit:cover;width:42px;height:42px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"style":{"layout":{"selfStretch":"fixed","flexSize":"300px"},"spacing":{"blockGap":"var:preset|spacing|20"},"elements":{"link":{"color":{"text":"var:preset|color|black-90"}}}},"textColor":"black-90","layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
+<div class="wp-block-group has-black-90-color has-text-color has-link-color"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|black-90"}}}},"textColor":"black-90","fontSize":"xs"} -->
+<p class="has-black-90-color has-text-color has-link-color has-xs-font-size"><?php /* Translators: 1. is the start of a 'code' HTML element, 2. is the end of a 'code' HTML element */ 
+echo sprintf( esc_html__( '★★★★★ %1$s4,8 (2k)%2$s', 'fitness-salzkammergut' ), '<code>', '</code>' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|black-90"}}}},"textColor":"black-90"} -->
+<p class="has-black-90-color has-text-color has-link-color"><?php /* Translators: 1. is a 'br' HTML element */ 
+echo sprintf( esc_html__( 'Dein VIVO in%1$sBad Goisern', 'fitness-salzkammergut' ), '<br>' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"200"},"elements":{"link":{"color":{"text":"var:preset|color|black-90"}}}},"textColor":"black-90","fontSize":"xs","fontFamily":"work-sans"} -->
+<p class="has-black-90-color has-text-color has-link-color has-work-sans-font-family has-xs-font-size" style="font-style:normal;font-weight:200"><?php esc_html_e('Mehr über VIVO', 'fitness-salzkammergut');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"width":"0px","style":{"layout":{"flexSize":"0px","selfStretch":"fixed"}}} -->
+<div style="height:100px;width:0px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e('►', 'fitness-salzkammergut');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"border":{"radius":"10px"}},"backgroundColor":"black-30","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group has-black-30-background-color has-background" style="border-radius:10px"><!-- wp:image {"width":"auto","height":"42px","sizeSlug":"full","linkDestination":"none","style":{"spacing":{"margin":{"top":"0","bottom":"0","left":"0","right":"0"}}}} -->
+<figure class="wp-block-image size-full is-resized" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/Vivo_Symbol_Lime.svg" alt="" class="" style="width:auto;height:42px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"style":{"layout":{"selfStretch":"fixed","flexSize":"300px"}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|lime"}}}},"textColor":"lime"} -->
+<p class="has-lime-color has-text-color has-link-color"><?php esc_html_e('★★★★★', 'fitness-salzkammergut');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e('Dein VIVO in', 'fitness-salzkammergut');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e('Bad Goisern', 'fitness-salzkammergut');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"xs"} -->
+<p class="has-xs-font-size"><?php esc_html_e('Mehr über VIVO', 'fitness-salzkammergut');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"width":"0px","style":{"layout":{"flexSize":"100px","selfStretch":"fixed"}}} -->
+<div style="height:100px;width:0px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p><?php esc_html_e('►', 'fitness-salzkammergut');?></p>
+<!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
 <!-- /wp:group --></div></div>
-<!-- /wp:cover -->
+<!-- /wp:cover --></div>
+<!-- /wp:group -->
