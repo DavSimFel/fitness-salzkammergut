36d4059562752a1263b98944b031b5fdab7d8cca
David Simon Feldhofer <david@feldhofer.cc>
2025-10-06 10:21:15 +0200

    dev

--- patterns/header.php
+++ patterns/header.php
@@ -8,7 +8,7 @@
 <!-- wp:group {"backgroundColor":"almost-black","layout":{"type":"default"}} -->
 <div class="wp-block-group has-almost-black-background-color has-background"><!-- wp:cover {"useFeaturedImage":true,"dimRatio":50,"overlayColor":"almost-black","isUserOverlayColor":true,"minHeight":100,"minHeightUnit":"vh","contentPosition":"center center","isDark":false,"sizeSlug":"full","align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|soft-white"}},"heading":{"color":{"text":"var:preset|color|soft-white","background":"var:preset|color|custom-transparent"}}}},"textColor":"soft-white","layout":{"type":"constrained"}} -->
 <div class="wp-block-cover aligncenter is-light has-soft-white-color has-text-color has-link-color" style="min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-almost-black-background-color has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"dimensions":{"minHeight":"100vh"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
-<div class="wp-block-group" style="min-height:100vh"><!-- wp:group {"metadata":{"name":"Navigation"},"style":{"elements":{"link":{"color":{"text":"var:preset|color|black-90"}}}},"textColor":"black-90","layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group" style="min-height:100vh"><!-- wp:group {"metadata":{"name":"Navigation"},"style":{"elements":{"link":{"color":{"text":"var:preset|color|black-90"}}}},"textColor":"black-90","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center"}} -->
 <div class="wp-block-group has-black-90-color has-text-color has-link-color"><!-- wp:group {"metadata":{"name":"Navigation Bar - Mobile"},"style":{"border":{"radius":"100px"},"spacing":{"padding":{"right":"var:preset|spacing|40","left":"var:preset|spacing|40"}}},"backgroundColor":"soft-white","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"},"blockVisibility":{"visibilityPresets":{"presets":[547]}}} -->
 <div class="wp-block-group has-soft-white-background-color has-background" style="border-radius:100px;padding-right:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:group {"metadata":{"patternName":"fitness-salzkammergut/linked-logo-row","name":"Linked Logo Row"},"style":{"layout":{"selfStretch":"fit","flexSize":null},"dimensions":{"minHeight":"48px"},"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"},"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between","verticalAlignment":"center"}} -->
 <div class="wp-block-group" style="min-height:48px;margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:image {"lightbox":{"enabled":false},"width":"auto","height":"24px","sizeSlug":"full","linkDestination":"custom","style":{"spacing":{"margin":{"top":"0","bottom":"0"}},"layout":{"selfStretch":"fill","flexSize":null}}} -->
@@ -29,7 +29,13 @@
 
 <!-- wp:group {"metadata":{"name":"Navigation Bar - Desktop/Tablet"},"style":{"border":{"radius":"100px"},"spacing":{"padding":{"right":"var:preset|spacing|60","left":"var:preset|spacing|60"}}},"backgroundColor":"soft-white","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"},"blockVisibility":{"visibilityPresets":{"presets":[557]}}} -->
 <div class="wp-block-group has-soft-white-background-color has-background" style="border-radius:100px;padding-right:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:navigation {"ref":430,"overlayMenu":"never","layout":{"type":"flex","flexWrap":"nowrap"},"blockVisibility":{"controlSets":[{"id":1,"enable":true,"controls":{"screenSize":{"hideOnScreenSize":{"small":false,"medium":false}}}}]}} /--></div>
-<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php esc_html_e('Kostenloses Probetraining', 'fitness-salzkammergut');?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
 <!-- /wp:group -->
 
 <!-- wp:spacer {"style":{"layout":{"selfStretch":"fill","flexSize":null}}} -->
@@ -59,17 +65,17 @@
 
 <!-- wp:column {"width":"50%","layout":{"type":"constrained","justifyContent":"right"}} -->
 <div class="wp-block-column" style="flex-basis:50%"><!-- wp:group {"style":{"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap","justifyContent":"right"}} -->
-<div class="wp-block-group"><!-- wp:group {"style":{"border":{"radius":"10px"},"spacing":{"blockGap":"var:preset|spacing|50","padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}},"elements":{"link":{"color":{"text":"var:preset|color|black-90"}}}},"backgroundColor":"black-30","textColor":"black-90","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:group {"metadata":{"name":"Gym Card - VIVO"},"style":{"border":{"radius":"10px"},"spacing":{"blockGap":"var:preset|spacing|50","padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}},"elements":{"link":{"color":{"text":"var:preset|color|black-90"}}}},"backgroundColor":"black-30","textColor":"black-90","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
 <div class="wp-block-group has-black-90-color has-black-30-background-color has-text-color has-background has-link-color" style="border-radius:10px;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--50)"><!-- wp:image {"width":"auto","height":"42px","sizeSlug":"full","linkDestination":"none","className":"tw-flex-none","style":{"spacing":{"margin":{"top":"0","bottom":"0","left":"0","right":"0"}},"layout":{"selfStretch":"fit","flexSize":null}}} -->
 <figure class="wp-block-image size-full is-resized tw-flex-none" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/Vivo_Symbol_Black.svg" alt="" style="width:auto;height:42px"/></figure>
 <!-- /wp:image -->
 
 <!-- wp:group {"style":{"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"blockGap":"var:preset|spacing|20","padding":{"top":"0","bottom":"0","left":"0","right":"var:preset|spacing|60"}},"elements":{"link":{"color":{"text":"var:preset|color|black-90"}}}},"textColor":"black-90","layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
-<div class="wp-block-group has-black-90-color has-text-color has-link-color" style="padding-top:0;padding-right:var(--wp--preset--spacing--60);padding-bottom:0;padding-left:0"><!-- wp:fitness/rating-badge {"placeId":"ChIJBcVrP6dLcUcRBuhInry255U","showLogo":false,"size":"small"} /-->
+<div class="wp-block-group has-black-90-color has-text-color has-link-color" style="padding-top:0;padding-right:var(--wp--preset--spacing--60);padding-bottom:0;padding-left:0"><!-- wp:fitness/rating-badge {"placeId":"ChIJBcVrP6dLcUcRBuhInry255U","showLogo":false,"size":"small","textColor":"black-60","fontSize":"xs","style":{"spacing":{"padding":{"top":"0","bottom":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|black-60"}}}}} /-->
 
 <!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|black-90"}}}},"textColor":"black-90"} -->
-<p class="has-black-90-color has-text-color has-link-color"><?php /* Translators: 1. is a 'br' HTML element */ 
-echo sprintf( esc_html__( 'Dein VIVO in%1$sBad Goisern', 'fitness-salzkammergut' ), '<br>' ); ?></p>
+<p class="has-black-90-color has-text-color has-link-color"><?php /* Translators: 1. is the start of a 'strong' HTML element, 2. is the end of a 'strong' HTML element, 3. is a 'br' HTML element */ 
+echo sprintf( esc_html__( 'Dein %1$sVIVO%2$s in%3$sBad Goisern', 'fitness-salzkammergut' ), '<strong>', '</strong>', '<br>' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"200"},"elements":{"link":{"color":{"text":"var:preset|color|black-90"}}}},"textColor":"black-90","fontSize":"xs","fontFamily":"work-sans"} -->
@@ -82,21 +88,21 @@ echo sprintf( esc_html__( 'Dein VIVO in%1$sBad Goisern', 'fitness-salzkammergut'
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
-<!-- wp:group {"style":{"border":{"radius":"10px"},"spacing":{"blockGap":"var:preset|spacing|50","padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}},"elements":{"link":{"color":{"text":"var:preset|color|black-90"}}}},"backgroundColor":"black-30","textColor":"black-90","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<!-- wp:group {"metadata":{"name":"Gym Card - ExcitingFit"},"style":{"border":{"radius":"10px"},"spacing":{"blockGap":"var:preset|spacing|50","padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}},"elements":{"link":{"color":{"text":"var:preset|color|black-90"}}}},"backgroundColor":"black-30","textColor":"black-90","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
 <div class="wp-block-group has-black-90-color has-black-30-background-color has-text-color has-background has-link-color" style="border-radius:10px;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--50)"><!-- wp:image {"width":"auto","height":"42px","sizeSlug":"full","linkDestination":"none","className":"tw-flex-none","style":{"spacing":{"margin":{"top":"0","bottom":"0","left":"0","right":"0"}},"layout":{"selfStretch":"fit","flexSize":null}}} -->
 <figure class="wp-block-image size-full is-resized tw-flex-none" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/Excitingfit_Symbol_Black.svg" alt="" style="width:auto;height:42px"/></figure>
 <!-- /wp:image -->
 
-<!-- wp:group {"style":{"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"blockGap":"var:preset|spacing|20","padding":{"top":"0","bottom":"0","left":"0","right":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|black-90"}}}},"textColor":"black-90","layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
-<div class="wp-block-group has-black-90-color has-text-color has-link-color" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:fitness/rating-badge {"placeId":"ChIJ59nsISpNcUcRIQ2RH8DwoAE","showLogo":false,"size":"small"} /-->
+<!-- wp:group {"style":{"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"blockGap":"var:preset|spacing|20","padding":{"top":"0","bottom":"0","left":"0","right":"var:preset|spacing|60"}},"elements":{"link":{"color":{"text":"var:preset|color|black-90"}}}},"textColor":"black-90","layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
+<div class="wp-block-group has-black-90-color has-text-color has-link-color" style="padding-top:0;padding-right:var(--wp--preset--spacing--60);padding-bottom:0;padding-left:0"><!-- wp:fitness/rating-badge {"placeId":"ChIJ59nsISpNcUcRIQ2RH8DwoAE","showLogo":false,"size":"small","textColor":"black-60","fontSize":"xs","style":{"spacing":{"padding":{"top":"0","bottom":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|black-60"}}}}} /-->
 
 <!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|black-90"}}}},"textColor":"black-90"} -->
-<p class="has-black-90-color has-text-color has-link-color"><?php /* Translators: 1. is a 'br' HTML element */ 
-echo sprintf( esc_html__( 'Dein ExcitingFit in%1$sBad Goisern', 'fitness-salzkammergut' ), '<br>' ); ?></p>
+<p class="has-black-90-color has-text-color has-link-color"><?php /* Translators: 1. is the start of a 'strong' HTML element, 2. is the end of a 'strong' HTML element, 3. is a 'br' HTML element */ 
+echo sprintf( esc_html__( 'Dein %1$sExcitingFit%2$s%3$sin Bad Ischl', 'fitness-salzkammergut' ), '<strong>', '</strong>', '<br>' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"200"},"elements":{"link":{"color":{"text":"var:preset|color|black-90"}}}},"textColor":"black-90","fontSize":"xs","fontFamily":"work-sans"} -->
-<p class="has-black-90-color has-text-color has-link-color has-work-sans-font-family has-xs-font-size" style="font-style:normal;font-weight:200"><?php esc_html_e('Mehr über VIVO', 'fitness-salzkammergut');?></p>
+<p class="has-black-90-color has-text-color has-link-color has-work-sans-font-family has-xs-font-size" style="font-style:normal;font-weight:200"><?php esc_html_e('Mehr über ExcitingFit', 'fitness-salzkammergut');?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
@@ -110,7 +116,13 @@ echo sprintf( esc_html__( 'Dein ExcitingFit in%1$sBad Goisern', 'fitness-salzkam
 
 <!-- wp:spacer {"style":{"layout":{"selfStretch":"fixed","flexSize":"100px"}}} -->
 <div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer --></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:fitness/review-feed {"placeIds":"ChIJ59nsISpNcUcRIQ2RH8DwoAE, ChIJBcVrP6dLcUcRBuhInry255U","limit":6,"maxLength":120,"fontSize":"xs","layout":{"type":"grid","columnCount":6,"minimumColumnWidth":null},"blockVisibility":{"visibilityPresets":{"presets":[557]}}} /-->
+
+<!-- wp:fitness/review-feed {"placeIds":"ChIJ59nsISpNcUcRIQ2RH8DwoAE, ChIJBcVrP6dLcUcRBuhInry255U","limit":2,"minRating":5,"maxLength":120,"fontSize":"xs","layout":{"type":"flex","justifyContent":"center"},"blockVisibility":{"visibilityPresets":{"presets":[547]}}} /--></div>
+<!-- /wp:group --></div>
 <!-- /wp:group --></div></div>
 <!-- /wp:cover --></div>
 <!-- /wp:group -->
